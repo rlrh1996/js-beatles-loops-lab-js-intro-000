3f0d417d7546a5207ffb316b4e650b83ff064301
rlrh1996 <rlrh1996@gmail.com>
2017-09-12 10:39:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ function theBeatlesPlay(musicians, instruments) {
     for (let i = 0; i < musicians.length; i++) {
         output.push(`${musicians[i]} plays ${instruments[i]}`)
     }
-    return output;
+    return output
 }
